d1f768721f00b6dda496b8586fa83d9f49b61c87
Xavier Clerc <xclerc@janestreet.com>
2023-10-17 14:36:52 +0100

    ocamlformat

--- backend/cfg/cfg.ml
+++ backend/cfg/cfg.ml
@@ -55,7 +55,7 @@ type t =
     mutable fun_contains_calls : bool;
     (* CR-someday gyorsh: compute locally. *)
     fun_num_stack_slots : int array;
-    fun_poll: Lambda.poll_attribute;
+    fun_poll : Lambda.poll_attribute
   }
 
 let create ~fun_name ~fun_args ~fun_dbg ~fun_fast ~fun_contains_calls
